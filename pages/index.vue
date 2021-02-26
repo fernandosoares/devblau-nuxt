b8574aefae96ad5a16a81f83c5ab3f2996c20d04
Fernando Soares <fernandosoares.dev@gmail.com>
2021-02-26 11:53:39 -0300

    Initial setup

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Hello, Nuxt!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: 'DevBlau | Games & Tech',
+    }
+  },
+}
+</script>
+
+<style></style>
