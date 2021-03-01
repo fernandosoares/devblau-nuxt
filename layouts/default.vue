68b68c9b89dcd30e4992e603140315d0860aea68
Fernando Soares <fernandosoares.dev@gmail.com>
2021-03-01 13:47:57 -0300

    Menu

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,17 @@
 <template>
   <div>
+    <Nav />
     <Nuxt />
   </div>
 </template>
-
+<script>
+import Nav from '../components/Nav'
+export default {
+  components: {
+    Nav,
+  },
+}
+</script>
 <style>
 body {
   margin: 0;
